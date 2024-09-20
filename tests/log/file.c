fee6d8953f0b31d49c746c25f144b296e0008a44
Jorge Ventura <venturasac@hotmail.com>
2024-09-20 15:02:47 +0200

    Adding test fot init()

--- tests/log/file.c
+++ tests/log/file.c
@@ -5,6 +5,8 @@
 #include "unity.h"
 /* TARGET LIBRARY */
 #include "file.h"
+#include <stdint.h>
+#include <unity_internals.h>
 
 /******************************************************************************/
 /*    PRELUDE                                                                 */
@@ -25,9 +27,47 @@ void tearDown(void)
 /*    TEST FUNCTIONS                                                          */
 /******************************************************************************/
 
+void test_init(void)
+{
+    /* Wrong (LOG*)1U value */
+    LOG *nullPtr = init((LOG*)1U, 0U);
+    TEST_ASSERT_EQUAL_PTR ((LOG*)1U, nullPtr);
+
+    /* only stderr as target for logging */
+    /* LOG_TO_FILE is either 0U or 1U */
+    LOG *files = init(file, 0U);
+    TEST_ASSERT_NOT_NULL(files);
+
+    uint32_t count = 0U;
+    for (LOG *itr = files; itr->name != NULL; itr = itr + sizeof(LOG))
+    {
+        TEST_ASSERT_NOT_NULL(itr);
+        TEST_ASSERT_EQUAL_STRING("stderr", itr->name);
+        TEST_ASSERT_EQUAL_PTR(stderr, itr->descriptor);
+        TEST_ASSERT_EQUAL_UINT32(0U, count);
+        count++;
+    }
+
+    /* a file and the stderr as targets for logging */
+    LOG *tmp = files = init(files, 1U);
+    TEST_ASSERT_NOT_NULL(files);
+
+    count = 0U;
+    for (LOG *itr = files; itr->name != NULL; itr = itr + sizeof(LOG))
+    {
+        TEST_ASSERT_NOT_NULL(itr);
+        TEST_ASSERT_NOT_NULL(itr->name);
+        TEST_ASSERT_NOT_NULL(itr->descriptor);
+        count++;
+    }
+    TEST_ASSERT_EQUAL_UINT32(1U, count);
+}
+
 int main(void)
 {
     UNITY_BEGIN();
+
+    RUN_TEST(test_init);
 
     return UNITY_END();
 }
