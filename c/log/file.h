1c428cf39fbf945bda7e38e74d0031e6a1534b59
Jorge Ventura <venturasac@hotmail.com>
2024-09-21 19:33:47 +0200

    file.h and its unit test

--- c/log/file.h
+++ c/log/file.h
@@ -23,6 +23,9 @@ extern "C" {
 
 #include <sys/stat.h>
 #include <errno.h>
+#include <time.h>
+
+#include "levels.h"
 
 /******************************************************************************/
 /*    DEFINITIONS                                                             */
@@ -46,6 +49,8 @@ typedef struct Log
 
 /* file handler */
 static LOG *file = NULL;
+
+#define MAX_NAME_LEN  (50U)
 
 /******************************************************************************/
 /*    IMPLEMENTATION                                                          */
@@ -87,6 +92,91 @@ int32_t make(const char *dir)
     }
 
     return errno;
+}
+
+int32_t name(LOG *file)
+{
+    int32_t ret;
+
+    file->name = malloc(sizeof(char) * MAX_STR_LEN);
+    if (file->name == NULL)
+    {
+        LOG_WARNING("Logging file was not created.");
+        ret = -1;
+    }
+    else
+    {
+        /* Creating dir */
+        ret = make("tmp");
+        if (ret > -1)
+        {
+            time_t secs;
+            /* Creating tmp name */
+            time(&secs);
+            memset(file->name, 0, sizeof(char) * MAX_NAME_LEN);
+            sprintf(file->name, "tmp/%ld.log", secs);
+        }
+    }
+
+    return ret;
+}
+
+LOG* open(LOG *file)
+{
+    LOG *ret = NULL;
+    file->descriptor = fopen(file->name, "w");
+    if (file->descriptor == NULL)
+    {
+        LOG_WARNING("File was not created! [CODE %d]", errno);
+        free(file->name);
+        free(file);
+
+        ret = NULL;
+    }
+    else
+    {
+        LOG_INFO("File %s was created.", file->name);
+        ret = file;
+    }
+
+    return ret;
+}
+
+void constructor(void)
+{
+    int32_t exists;
+
+    file = init(file, LOG_TO_FILE);
+    exists = name(file);
+    if (exists > -1)
+    {
+        open(file);
+    }
+
+    return;
+}
+
+void destructor(void)
+{
+    if (file != NULL)
+    {
+        for (LOG *itr = file; itr->name != NULL; itr++)
+        {
+            printf("Closing file %s, %p\n", itr->name, itr->name);
+            fflush(itr->descriptor);
+            fclose(itr->descriptor);
+            if (strcmp(itr->name, "stderr") != 0)
+            {
+                free(itr->name);
+                itr->descriptor = NULL;
+                itr->name = NULL;
+            }
+        }
+    }
+
+    file = NULL;
+
+    return;
 }
 
 #ifdef __cplusplus
--- tests/log/file.c
+++ tests/log/file.c
@@ -2,6 +2,8 @@
 /*    INCLUDED FILES                                                          */
 /******************************************************************************/
 
+#include <stdio.h>
+#include <time.h>
 #include <unistd.h>
 
 #include "unity.h"
@@ -33,34 +35,29 @@ void test_init(void)
     LOG *nullPtr = init((LOG*)1U, 0U);
     TEST_ASSERT_EQUAL_PTR ((LOG*)1U, nullPtr);
 
-    /* only stderr as target for logging */
     /* LOG_TO_FILE is either 0U or 1U */
-    LOG *files = init(file, 0U);
-    TEST_ASSERT_NOT_NULL(files);
+    /* len(file) == 2U */
+    file = init(file, 0U);
+    TEST_ASSERT_NOT_NULL(file);
+    TEST_ASSERT_EQUAL_STRING("stderr", file[0U].name);
+    TEST_ASSERT_EQUAL_PTR(stderr, file[0U].descriptor);
+    TEST_ASSERT_NULL(file[1U].name);
+    TEST_ASSERT_NULL(file[1U].descriptor);
+    free(file);
+    file = NULL;
 
-    uint32_t count = 0U;
-    for (LOG *itr = files; itr->name != NULL; itr = itr + sizeof(LOG))
-    {
-        TEST_ASSERT_NOT_NULL(itr);
-        TEST_ASSERT_EQUAL_STRING("stderr", itr->name);
-        TEST_ASSERT_EQUAL_PTR(stderr, itr->descriptor);
-        TEST_ASSERT_EQUAL_UINT32(0U, count);
-        count++;
-    }
+    /* len(file) == 3U */
+    file = init(file, 1U);
+    TEST_ASSERT_NOT_NULL(file);
+    TEST_ASSERT_NULL(file[0U].name);
+    TEST_ASSERT_NULL(file[0U].descriptor);
+    TEST_ASSERT_EQUAL_STRING("stderr", file[1U].name);
+    TEST_ASSERT_EQUAL_PTR(stderr, file[1U].descriptor);
+    TEST_ASSERT_NULL(file[2U].name);
+    TEST_ASSERT_NULL(file[2U].descriptor);
+    free(file);
+    file = NULL;
 
-    /* a file and the stderr as targets for logging */
-    LOG *tmp = files = init(files, 1U);
-    TEST_ASSERT_NOT_NULL(files);
-
-    count = 0U;
-    for (LOG *itr = files; itr->name != NULL; itr = itr + sizeof(LOG))
-    {
-        TEST_ASSERT_NOT_NULL(itr);
-        TEST_ASSERT_NOT_NULL(itr->name);
-        TEST_ASSERT_NOT_NULL(itr->descriptor);
-        count++;
-    }
-    TEST_ASSERT_EQUAL_UINT32(1U, count);
 }
 
 void test_make(void)
@@ -73,12 +70,66 @@ void test_make(void)
     remove("dummy");
 }
 
+void test_name(void)
+{
+    LOG newFile;
+    time_t secs;
+    char filename[50U];
+
+    time(&secs);
+    name(&newFile);
+    sprintf(filename, "tmp/%ld.log", secs);
+    TEST_ASSERT_EQUAL_STRING(filename, newFile.name);
+
+    remove("tmp");
+}
+
+void test_open(void)
+{
+    char filename[] = "test.log";
+    LOG newFile = {NULL, filename};
+    TEST_ASSERT_EQUAL_PTR(&newFile, open(&newFile));
+}
+
+void test_constructor(void)
+{
+    /* LOG_TO_FILE is 1U due to the buildsystem */
+    time_t secs;
+    char filename[50U];
+
+    /* Trying to get the same name, race condition */
+    time(&secs);
+    sprintf(filename, "tmp/%ld.log", secs);
+
+    constructor();
+    TEST_ASSERT_EQUAL_STRING(filename, file[0U].name);
+    TEST_ASSERT_NOT_NULL(file[0U].descriptor);
+    TEST_ASSERT_EQUAL_STRING("stderr", file[1U].name);
+    TEST_ASSERT_EQUAL_PTR(stderr, file[1U].descriptor);
+
+    free(file[0U].name);
+    free(file);
+    file = NULL;
+}
+
+void test_destructor(void)
+{
+    constructor();
+
+    destructor();
+    TEST_ASSERT_NULL(file);
+}
+
 int main(void)
 {
     UNITY_BEGIN();
 
     RUN_TEST(test_init);
     RUN_TEST(test_make);
+    RUN_TEST(test_name);
+    RUN_TEST(test_open);
+    RUN_TEST(test_constructor);
+    RUN_TEST(test_destructor);
 
     return UNITY_END();
 }
