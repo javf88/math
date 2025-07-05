789430033f94dabcb224a099bb668c3d134704ac
Jorge Ventura <venturasac@hotmail.com>
2025-07-05 17:49:25 +0200

    Refactoring memory.hpp

--- cpp/src/memory.hpp
+++ cpp/src/memory.hpp
@@ -1,32 +1,57 @@
-#include <iostream>
-#include <cstdint>
-#include <string>
+/*******************************************************************************
+*
+* MEMORY MANAGEMENT SYSTEM
+*
+*   SUMMARY
+*       This single-header module implements a simple but effective memory
+*       management system, the M2S. It extends/generalizes the vector container
+*       to a matrix container.
+*
+*       The API's functionality is simple:
+*       a) matrix creation
+*       b) matrix destruction
+*
+*******************************************************************************/
+
+#ifndef MEMORY_H_
+#define MEMORY_H_
+
+/******************************************************************************/
+/*    INCLUDED FILES                                                          */
+/******************************************************************************/
+
+#include <initializer_list>
 #include <sstream>
+#include <vector>
+
+/******************************************************************************/
+/*    API                                                                     */
+/******************************************************************************/
 
 struct Matrix
 {
-    Matrix(uint32_t rows, uint32_t cols) : rows(rows), cols(cols)
+    uint32_t    rows = 0;
+    uint32_t    cols = 0;
+    std::vector<float>  val;
+
+    // Memory allocation for a matrix
+    Matrix(uint32_t rows, uint32_t cols);
+
+    // Empty matrix
+    Matrix();
+
+    // Matrix allocation from a list
+    Matrix(std::initializer_list<float> val);
+
+    /*Matrix& transpose()
     {
-        if (this->rows * this->cols != 0)
+        Matrix *AT = nullptr;
+        if (this->val == nullptr)
         {
-            this->val = new float[this->rows * this->cols];
+            //LOG_WARNING("Nothing to transpose: Empty matrix!");
+            return  *this;
         }
-        else
-        {
-            this->val = NULL;
-        }
-    }
-
-    Matrix() : Matrix(0, 0) {};
-
-
-    ~Matrix()
-    {
-        if (this->val != NULL)
-        {
-            delete this->val;
-        }
-    }
+    }*/
 
 
     operator std::string() const
@@ -34,12 +59,47 @@ struct Matrix
         std::ostringstream out;
 
         out << "(MATRIX) in [" << this->rows <<"x" << this->cols << "]" << std::endl;
+        if (this->rows * this->cols == 0)
+        {
+            out << "\t[] empty matrix" << std::endl;
+        }
+        else
+        {
+            for (uint32_t row = 0; row < this->rows; row++)
+            {
+                out << "\t[ ";
+                for (uint32_t col = 0; col < this->cols; col++)
+                {
+                    out << " " << std::to_string(this->val[col]) << ",";
+                }
+                out << "]" << std::endl;
+            }
+        }
 
         return out.str();
     }
-
-    private:
-        uint32_t rows;
-        uint32_t cols;
-        float   *val;
 };
+
+/******************************************************************************/
+/*    IMPLEMENTATION                                                          */
+/******************************************************************************/
+
+// Matrix allocation from a list
+Matrix::Matrix(std::initializer_list<float> val) : val(val), rows(1), cols(val.size()) {}
+
+Matrix::Matrix(uint32_t rows, uint32_t cols) : rows(rows), cols(cols)
+{
+    if (this->rows * this->cols != 0)
+    {
+        this->val.reserve(this->rows * this->cols);
+    }
+    else
+    {
+        // Empty else
+    }
+}
+
+// Empty matrix
+Matrix::Matrix() : Matrix(0, 0) {}
+
+#endif /* MEMORY_H_ */
--- cpp/tests/memory.cpp
+++ cpp/tests/memory.cpp
@@ -6,12 +6,59 @@
 /* TARGET LIBRARY */
 #include "memory.hpp"
 
+
+class DummyData: public testing::Test
+{
+    private:
+        Matrix A;
+        uint32_t step = 1;
+        uint32_t init = 0;
+
+    public:
+
+        void SetUp() override
+        {
+            using namespace std;
+
+            cout << "Running SetUp()" << endl;
+
+            for (uint32_t i = 0; i < A.rows; i++)
+            {
+                for (uint32_t j = 0; j < A.cols; j++)
+                {
+                    cout << i * A.cols + step << ", ";
+                }
+
+                cout << endl;
+            }
+        }
+};
+
 TEST(Matrix, Constructors)
 {
-    Matrix A;
-    Matrix B(3,5);
+    Matrix Empty;
+    ASSERT_EQ(0, Empty.rows);
+    ASSERT_EQ(0, Empty.cols);
+    ASSERT_EQ(true, Empty.val.empty());
+
+    Matrix A(3,5);
+    ASSERT_EQ(3, A.rows);
+    ASSERT_EQ(5, A.cols);
+    ASSERT_EQ(A.rows * A.cols, A.val.capacity());
+
+    Matrix B({1, 2, 3, 4, 5, 6, 7});
+    ASSERT_EQ(1, B.rows);
+    ASSERT_EQ(7, B.cols);
+    ASSERT_EQ(B.rows * B.cols, B.val.capacity());
 
     using namespace std;
+    cout << string(Empty) << endl;
     cout << string(A) << endl;
     cout << string(B) << endl;
+}
+
+TEST_F(DummyData, SetUp)
+{
+    using namespace std;
+//    cout << string(this->A) << endl;
 }
