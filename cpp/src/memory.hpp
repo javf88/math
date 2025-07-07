38125bf09d681b87b2a356683a37138633d82414
Jorge Ventura <venturasac@hotmail.com>
2025-07-07 17:50:50 +0200

    Adding reshape method and its tests

--- cpp/src/memory.hpp
+++ cpp/src/memory.hpp
@@ -20,6 +20,7 @@
 /*    INCLUDED FILES                                                          */
 /******************************************************************************/
 
+#include <cstdint>
 #include <initializer_list>
 #include <sstream>
 #include <vector>
@@ -34,6 +35,7 @@ struct Matrix
     uint32_t    cols = 0;
     std::vector<float>  val;
 
+    // Constructors & Destructors
     // Memory allocation for a matrix
     Matrix(uint32_t rows, uint32_t cols);
 
@@ -43,15 +45,73 @@ struct Matrix
     // Matrix allocation from a list
     Matrix(std::initializer_list<float> val);
 
-    /*Matrix& transpose()
+    // Operators
+    Matrix& reshape(uint32_t newRows, uint32_t newCols)
+    {
+        uint32_t total = this->rows * this->cols;
+        uint32_t newTotal = newRows * newCols;
+
+        if (total > newTotal)
+        {
+            std::printf("It is not possible to reshape from [%ux%u] to [%ux%u]\n",
+                    this->rows, this->cols, newRows, newCols);
+        }
+        else
+        {
+            this->rows = newRows;
+            this->cols = newCols;
+
+            this->val.resize(newTotal);
+        }
+
+        return *this;
+    }
+
+    Matrix& transpose()
     {
         Matrix *AT = nullptr;
-        if (this->val == nullptr)
+        if (this->val.empty())
         {
             //LOG_WARNING("Nothing to transpose: Empty matrix!");
             return  *this;
         }
-    }*/
+        else
+        {
+            // Swapping dimensions
+            //uint32_t tmp = this->rows;
+            //this->rows = this->cols;
+            //this->cols = tmp;
+
+            // There is no need to transpose a row or a column vector. Their
+            // memory layout is the same.
+            if ((this->rows == 0) || (this->cols == 0))
+            {
+                return *this;
+            }
+            else
+            {
+                for (uint32_t row = 0; row < this->rows; row++)
+                {
+                    for (uint32_t col = 0; col < this->cols; col++)
+                    {
+                        uint32_t pos = this->cols * row + col;
+                        float val = this->val[pos];
+
+                        uint32_t newPos = this->rows * col + row;
+                        float newVal = this->val[newPos];
+
+                        this->val[newPos] = val;
+                        this->val[pos] = newVal;
+                    }
+                }
+                uint32_t tmp = this->cols;
+                this->cols = this->rows;
+                this->rows = tmp;
+
+                return *this;
+            }
+        }
+    }
 
 
     operator std::string() const
@@ -70,7 +130,8 @@ struct Matrix
                 out << "\t[ ";
                 for (uint32_t col = 0; col < this->cols; col++)
                 {
-                    out << " " << std::to_string(this->val[col]) << ",";
+                    uint32_t pos = this->cols * row + col;
+                    out << " " << std::to_string(this->val[pos]) << ",";
                 }
                 out << "]" << std::endl;
             }
