6e115d5790d5ba3cbfe5d58aeb6cbdaa9ef3f06f
Jorge Ventura <venturasac@hotmail.com>
2025-07-16 16:51:32 +0200

    Adding Static::stack and its tests

--- cpp/src/memory.hpp
+++ cpp/src/memory.hpp
@@ -3,13 +3,8 @@
 * MEMORY MANAGEMENT SYSTEM
 *
 *   SUMMARY
-*       This single-header module implements a simple but effective memory
-*       management system, the M2S. It extends/generalizes the vector container
-*       to a matrix container.
-*
-*       The API's functionality is simple:
-*       a) matrix creation
-*       b) matrix destruction
+*       A simple static stack to bookkeep the pointers of all dynamically
+*       allocated memory with the matrix.
 *
 *******************************************************************************/
 
@@ -20,147 +15,29 @@
 /*    INCLUDED FILES                                                          */
 /******************************************************************************/
 
-#include <cstdint>
-#include <initializer_list>
-#include <sstream>
-#include <vector>
+#include <stack>
 
 /******************************************************************************/
 /*    API                                                                     */
 /******************************************************************************/
 
-struct Matrix
+namespace Static
 {
-    uint32_t    rows = 0;
-    uint32_t    cols = 0;
-    std::vector<float>  val;
+    // std::stack has pop() and push() capabilities already.
+    static std::stack<void*> stack;
 
-    // Constructors & Destructors
-    // Memory allocation for a matrix
-    Matrix(uint32_t rows, uint32_t cols);
-
-    // Empty matrix
-    Matrix();
-
-    // Matrix allocation from a list
-    Matrix(std::initializer_list<float> val);
-
-    // Operators
-    Matrix& reshape(uint32_t newRows, uint32_t newCols)
+    std::stack<void*>* getStack()
     {
-        uint32_t total = this->rows * this->cols;
-        uint32_t newTotal = newRows * newCols;
-
-        if (total > newTotal)
-        {
-            std::printf("It is not possible to reshape from [%ux%u] to [%ux%u]\n",
-                    this->rows, this->cols, newRows, newCols);
-        }
-        else
-        {
-            this->rows = newRows;
-            this->cols = newCols;
-
-            this->val.resize(newTotal);
-        }
-
-        return *this;
+        return &stack;
     }
 
-    Matrix& transpose()
+    void clean()
     {
-        Matrix *AT = nullptr;
-        if (this->val.empty())
+        while (stack.empty() == false)
         {
-            //LOG_WARNING("Nothing to transpose: Empty matrix!");
-            return  *this;
+            stack.pop();
         }
-        else
-        {
-            // Swapping dimensions
-            //uint32_t tmp = this->rows;
-            //this->rows = this->cols;
-            //this->cols = tmp;
-
-            // There is no need to transpose a row or a column vector. Their
-            // memory layout is the same.
-            if ((this->rows == 0) || (this->cols == 0))
-            {
-                return *this;
-            }
-            else
-            {
-                for (uint32_t row = 0; row < this->rows; row++)
-                {
-                    for (uint32_t col = 0; col < this->cols; col++)
-                    {
-                        uint32_t pos = this->cols * row + col;
-                        float val = this->val[pos];
-
-                        uint32_t newPos = this->rows * col + row;
-                        float newVal = this->val[newPos];
-
-                        this->val[newPos] = val;
-                        this->val[pos] = newVal;
-                    }
-                }
-                uint32_t tmp = this->cols;
-                this->cols = this->rows;
-                this->rows = tmp;
-
-                return *this;
-            }
-        }
-    }
-
-
-    operator std::string() const
-    {
-        std::ostringstream out;
-
-        out << "(MATRIX) in [" << this->rows <<"x" << this->cols << "]" << std::endl;
-        if (this->rows * this->cols == 0)
-        {
-            out << "\t[] empty matrix" << std::endl;
-        }
-        else
-        {
-            for (uint32_t row = 0; row < this->rows; row++)
-            {
-                out << "\t[ ";
-                for (uint32_t col = 0; col < this->cols; col++)
-                {
-                    uint32_t pos = this->cols * row + col;
-                    out << " " << std::to_string(this->val[pos]) << ",";
-                }
-                out << "]" << std::endl;
-            }
-        }
-
-        return out.str();
-    }
-};
-
-/******************************************************************************/
-/*    IMPLEMENTATION                                                          */
-/******************************************************************************/
-
-// Matrix allocation from a list
-Matrix::Matrix(std::initializer_list<float> val) : val(val), rows(1), cols(val.size()) {}
-
-Matrix::Matrix(uint32_t rows, uint32_t cols) : rows(rows), cols(cols)
-{
-    if (this->rows * this->cols != 0)
-    {
-        this->val.reserve(this->rows * this->cols);
-    }
-    else
-    {
-        // Empty else
     }
 }
-
-// Empty matrix
-Matrix::Matrix() : Matrix(0, 0) {}
 
 #endif /* MEMORY_H_ */
--- cpp/tests/memory.cpp
+++ cpp/tests/memory.cpp
@@ -2,92 +2,54 @@
 /*    INCLUDED FILES                                                          */
 /******************************************************************************/
 
+#include <cstdint>
 #include <gtest/gtest.h>
 /* TARGET LIBRARY */
 #include "memory.hpp"
 
+/******************************************************************************/
+/*    FIXTURES                                                                */
+/******************************************************************************/
 
 class DummyData: public testing::Test
 {
-    private:
-        Matrix A;
-        uint32_t step = 1;
-        uint32_t init = 0;
+public:
+    void SetUp() override
+    {
+        using namespace std;
 
-    public:
+        // To improve logging message.
+        cout << "Running SetUp()" << endl;
 
-        void SetUp() override
+        stack<void*> *pStack = Static::getStack();
+        for (uint32_t i = 0U; i < 32U; i++)
         {
-            using namespace std;
-
-            cout << "Running SetUp()" << endl;
-
-            for (uint32_t i = 0; i < A.rows; i++)
-            {
-                for (uint32_t j = 0; j < A.cols; j++)
-                {
-                    cout << i * A.cols + step << ", ";
-                }
-
-                cout << endl;
-            }
+            void *pInt = new int(i);
+            pStack->push(pInt);
         }
+    }
 };
 
-TEST(Matrix, Constructors)
+/******************************************************************************/
+/*    TEST CASES                                                              */
+/******************************************************************************/
+
+TEST(stack, getStack)
 {
-    using namespace std;
+    std::stack<void*> *pStack = nullptr;
+    ASSERT_EQ(pStack, nullptr);
 
-    Matrix Empty;
-    ASSERT_EQ(0, Empty.rows);
-    ASSERT_EQ(0, Empty.cols);
-    ASSERT_EQ(true, Empty.val.empty());
-    cout << string(Empty) << endl;
-
-    Matrix A(3,5);
-    ASSERT_EQ(3, A.rows);
-    ASSERT_EQ(5, A.cols);
-    ASSERT_EQ(A.rows * A.cols, A.val.capacity());
-    cout << string(A) << endl;
-
-    Matrix B({1, 2, 3, 4, 5, 6, 7});
-    ASSERT_EQ(1, B.rows);
-    ASSERT_EQ(7, B.cols);
-    ASSERT_EQ(B.rows * B.cols, B.val.capacity());
-    cout << string(B) << endl;
-}
-
-TEST(Matrix, reshape)
-{
-    using namespace std;
-
-    Matrix A({0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3});
-
-    A.reshape(2,3);
-    ASSERT_EQ(1, A.rows);
-    ASSERT_EQ(12, A.cols);
-    cout << string(A) << endl;
-
-    A.reshape(4,3);
-    ASSERT_EQ(4, A.rows);
-    ASSERT_EQ(3, A.cols);
-    cout << string(A) << endl;
-
-}
-
-TEST(Matrix, transpose)
-{
-    using namespace std;
-
-    Matrix A({1, 2, 3, 4, 5, 6, 7});
-    cout << string(A) << endl;
-
-    A.transpose();
-    cout << string(A) << endl;
+    pStack = Static::getStack();
+    ASSERT_NE(pStack, nullptr);
 }
 
 TEST_F(DummyData, SetUp)
 {
-    using namespace std;
-//    cout << string(this->A) << endl;
+    std::stack<void*> *pStack = Static::getStack();
+    ASSERT_NE(pStack, nullptr);
+    ASSERT_EQ(pStack->empty(), false);
+
+    Static::clean();
+    ASSERT_EQ(pStack->empty(), true);
+    ASSERT_EQ(pStack->size(), 0U);
 }
