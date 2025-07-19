221846140b101e89feb61f2bbd7a3b49a433377b
Jorge Ventura <venturasac@hotmail.com>
2025-07-19 14:42:52 +0200

    Debugging matrix multiplication

--- cpp/src/operators.hpp
+++ cpp/src/operators.hpp
@@ -68,4 +68,96 @@ Matrix* operator+(const Matrix& A, const Matrix& B)
     return C;
 }
 
+Matrix* operator-(const Matrix& A, const Matrix &B)
+{
+    Matrix *C = nullptr;
+
+    if ((A.rows != B.rows) || (A.cols != B.cols))
+    {
+        std::fprintf(stderr, "Matrices A[%hu,%hu] and B[%hu,%hu] cannot be substracted.\n",
+                A.rows, A.cols, B.rows,B.cols);
+
+    }
+    else
+    {
+        C = new Matrix(A.rows, A.cols);
+
+        for (uint32_t i = 0U; i < A.rows; i++)
+        {
+            for (uint32_t j = 0U; j < A.cols; j++)
+            {
+                uint32_t pos = C->cols * i + j;
+                C->val[pos] = A.val[pos] - B.val[pos];
+            }
+        }
+    }
+
+    return C;
+}
+
+Matrix* operator*(const Matrix &A, const Matrix &B)
+{
+    Matrix *C = nullptr;
+
+    if (A.cols != B.rows)
+    {
+        std::printf("Matrices dimensions do not match [%ux%u] * [%ux%u]\n", A.rows, A.cols, B.rows, B.cols);
+
+        return nullptr;
+    }
+    else
+    {
+        C = new Matrix(A.rows, B.cols);
+        std::cout << std::string(B) << std::endl;
+        std::cout << std::string(A) << std::endl;
+        std::cout << std::string(*C) << std::endl;
+
+        for (uint32_t row = 0; row < C->rows; row++)
+        {
+            uint32_t i = C->cols * row;
+
+            for (uint32_t col = 0; col < C->cols; col++)
+            {
+                uint32_t pos = i + col;
+                std::fprintf(stderr, "(i,j) = (%hu,%hu) = %hu\n", i, col, pos);
+
+                C->val[pos] = 0.0F;
+
+                for (uint32_t k = 0; k < A.cols; k++)
+                {
+                    uint32_t j = B.cols * k + col;
+
+                    std::fprintf(stderr, "[pos] <- [i,k]*[k,j] = %hu <- [%hu,%hu]*[%hu,%hu]\n",
+                            pos, i, k, B.cols * k, col);
+                    C->val[pos] += A.val[i + k] * B.val[j];
+                }
+            }
+
+            std::cout << std::string(*C) << std::endl;
+        }
+    }
+
+    return C;
+}
+
+// implicit conversion from ints to floats
+Matrix* operator*(const float a, Matrix &B)
+{
+    Matrix *C = new Matrix;
+    C->reshape(B.rows, B.cols);
+
+    for (auto b = B.val.begin(), c = C->val.begin(); b != B.val.end(); b++, c++)
+    {
+        *c = a * *b;
+    }
+
+    return C;
+}
+
+Matrix* operator*(Matrix &A, const float b)
+{
+    Matrix *C = b * A;
+
+    return C;
+}
 #endif /* OPERATORS_H_ */
--- cpp/tests/operators.cpp
+++ cpp/tests/operators.cpp
@@ -40,7 +40,8 @@ TEST(operators, equality)
     Matrix B;
     A.id(2U);
     B.id(3U);
-    ASSERT_EQ(false, A == B);
+    ASSERT_FALSE(A == B);
+
     A.id(3);
     ASSERT_EQ(A, B);
 }
@@ -69,5 +70,104 @@ TEST(operators, add)
     D.reshape(2U, 4U);
     ASSERT_EQ(D, *C);
 
+    Static::clean();
+}
+
+TEST(operators, substract)
+{
+    Matrix A;
+    ASSERT_EQ(0U, A.rows);
+    ASSERT_EQ(0U, A.cols);
+    ASSERT_EQ(0U, A.val.capacity());
+
+    Matrix B({1,2,3,4,5,6,7,8});
+    ASSERT_EQ(1U, B.rows);
+    ASSERT_EQ(8U, B.cols);
+    ASSERT_EQ(8U, B.val.size());
+
+    Matrix *C = A - B;
+    ASSERT_EQ(nullptr, C);
+
+    B.reshape(2U, 4U);
+    C = B - B;
+    ASSERT_EQ(2U, C->rows);
+    ASSERT_EQ(4U, C->cols);
+
+    Matrix D(2U, 4U);
+    ASSERT_EQ(D, *C);
+
+    Static::clean();
+}
+
+TEST(operators, scalar)
+{
+    Matrix B({1,2,3,4,5,6,7,8});
+    B.reshape(2U, 4U);
+    ASSERT_EQ(2U, B.rows);
+    ASSERT_EQ(4U, B.cols);
+
+    Matrix *C = 4.0F * B;
+    ASSERT_EQ(2U, C->rows);
+    ASSERT_EQ(4U, C->cols);
+
+    Matrix D({4,8,12,16,20,24,28,32});
+    D.reshape(2U, 4U);
+    ASSERT_EQ(2U, D.rows);
+    ASSERT_EQ(4U, D.cols);
+    ASSERT_EQ(D, *C);
+
+    Matrix *E = B * 4.0;
+    ASSERT_EQ(D, *E);
+
+    Matrix *F = 4 * B;
+    ASSERT_EQ(D, *F);
+
+    Matrix *G = B * 4U;
+    ASSERT_EQ(D, *G);
+
+    Static::clean();
+}
+
+TEST(operators, multiply)
+{
+    Matrix A;
+    ASSERT_EQ(0U, A.rows);
+    ASSERT_EQ(0U, A.cols);
+    ASSERT_EQ(0U, A.val.capacity());
+
+    Matrix B({1,2,3,4,5,6,7,8,9,10,11,12});
+    B.reshape(3U, 4U);
+    ASSERT_EQ(3U, B.rows);
+    ASSERT_EQ(4U, B.cols);
+    ASSERT_EQ(12U, B.val.size());
+
+    Matrix *C = A * B;
+    ASSERT_EQ(nullptr, C);
+
+    A.id(3U);
+    ASSERT_EQ(3U, A.rows);
+    ASSERT_EQ(3U, A.cols);
+
+    C = 3 * A;
+    ASSERT_EQ(3U, C->rows);
+    ASSERT_EQ(3U, C->cols);
+    Matrix D({3,0,0,0,3,0,0,0,3});
+    D.reshape(3U, 3U);
+    ASSERT_EQ(D, *C);
+    std::cout << std::string(B) << std::endl;
+    std::cout << std::string(*C) << std::endl;
+    Matrix *E = *C * B;
+    ASSERT_EQ(3U, E->rows);
+    ASSERT_EQ(4U, E->cols);
+    std::cout << std::string(*E) << std::endl;
+
+/*
+    Matrix F({3,6,9,12,15,18,21,24,27,30,33,36});
+    F.reshape(3U, 4U);
+    std::cout << std::string(F) << std::endl;
+    ASSERT_EQ(3U, F.rows);
+    ASSERT_EQ(4U, F.cols);
+    ASSERT_EQ(F, *E);
+*/
     Static::clean();
 }
